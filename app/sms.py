86af2ad1986e159d31f732eaa62c3cd7853ec08a
Kirk gleason <kgleason@bloommg.com>
2014-05-16 17:37:42 -0400

    Made some of the SMS processing more clear. Added a check to make sure we don't add duplicates.

--- app/sms.py
+++ app/sms.py
@@ -18,7 +18,7 @@ def process_sms(r):
     
     if words[0].lower() == "#invite":
         if from_number in CONFIG['ADMIN_MOBILE_NUMBERS']:
-            invite_new_user(person=person, txt=words[1:])
+            return invite_new_user(person=person, txt=words[1:])
         else:
             return "You are not an admin. You are not allowed to invite new users."
     else:
@@ -28,12 +28,20 @@ def process_sms(r):
         return "Added \"{0}\" to the list".format(text)
         
 def invite_new_user(person, txt):
-    p = Person(firstname = txt[0], lastname = txt[1], mobile = "+1{0}".format(txt[2]))
-    db.session.add(p)
-    db.session.commit()
+    fname = txt[0]
+    lname = txt[1]
+    mbl = "+1{0}".format(txt[2])
+    p = Person.query.filter(Person.mobile == mbl).first()
+    if not p:
+        p = Person(firstname = fname, lastname = lname, mobile = mbl)
+        db.session.add(p)
+        db.session.commit()
     
-    send_sms(person=p, msg="Please save me in your contacts as Shopping List. If you text things to me, I will add them to the shopping list.")
-    return "Successfully added {0}.".format(p.display_name)
+        send_sms(person=p, msg="Please save me in your contacts as Shopping List. If you text things to me, I will add them to the shopping list.")
+        send_sms(person=p, msg="You can view the current shopping list at {0}".format(CONFIG['SHOPPING_LIST_URL']))
+        return "Successfully added {0}.".format(p.display_name)
+    else:
+        return "{0} already exists with mobile number {1}".format(p.display_name, p.mobile)
     
 def send_sms(person, msg):
     
