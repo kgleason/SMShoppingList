9138fc079d034060399303db14ce03e73ee8cdeb
Kirk gleason <kgleason@bloommg.com>
2014-05-16 13:34:19 -0400

    Added all of the views, I think

--- app/views.py
+++ app/views.py
@@ -1,7 +1,25 @@
 import os
 from flask import render_template, request
 from app import app
+from models import *
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', my_list=ListItem.all_open())
+    
+@app.route('/people')
+def people():
+    return render_template('people.html', people=Person.all())
+    
+@app.route('/help')
+def help():
+    return render_template('help.html')
+    
+@app.route('/person/<int:id>')
+def person(id):
+    p = Person.query.filter(Person.id == id).first()
+    return render_template('person.html', person=p)
+    
+@app.route('/sms' methods=['GET', 'POST'])
+def sms():
+    pass
