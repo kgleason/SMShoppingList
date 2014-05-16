9f1bdc8a1988efb551be3eb85ab84cdf794c8800
Kirk gleason <kgleason@bloommg.com>
2014-05-16 11:30:23 -0400

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate
+from config import CONFIG
+ 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = CONFIG['DBURI']
+ 
+db = SQLAlchemy()
+db.app = app
+db.init_app(app)
+ 
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+from app import models, views
---
+++ app/views.py
@@ -0,0 +1,7 @@
+import os
+from flask import render_template, request
+from app import app
+
+@app.route('/')
+def index():
+    return render_template('index.html')
