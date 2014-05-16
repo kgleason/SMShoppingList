fc1fa80afdc1363e92a109c58719de1f95523f29
Kirk gleason <kgleason@bloommg.com>
2014-05-16 13:52:00 -0400

    Added list item history as a parameter for the person view

--- app/views.py
+++ app/views.py
@@ -18,7 +18,10 @@ def help():
 @app.route('/person/<int:id>')
 def person(id):
     p = Person.query.filter(Person.id == id).first()
-    return render_template('person.html', person=p)
+    h = None
+    if p:
+        h = ListItem.query.filter(ListItem.created_by == p.id).order_by(desc(ListItem.id)).limit(50)
+    return render_template('person.html', person=p, history=h)
     
 @app.route('/sms', methods=['GET', 'POST'])
 def sms():
