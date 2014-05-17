08ccf2eb01573a8f0e06c24f4674370561615f93
Kirk gleason <kgleason@bloommg.com>
2014-05-16 20:07:35 -0400

    Fixed some regex substitutions, cleaned up some typos. Put the Exception output to the console.

--- app/sms.py
+++ app/sms.py
@@ -18,7 +18,7 @@ def process_sms(r):
     
     if words[0].lower() == "#invite":
         if from_number in CONFIG['ADMIN_MOBILE_NUMBERS']:
-            return invite_new_user(person=person, txt=words[1:])
+            return invite_new_user(inviter=person, txt=words[1:])
         else:
             return "You are not an admin. You are not allowed to invite new users."
     else:
@@ -27,32 +27,32 @@ def process_sms(r):
         db.session.commit()
         return "Added \"{0}\" to the list".format(text)
         
-def invite_new_user(person, txt):
+def invite_new_user(inviter, txt):
     fname = txt[0]
     lname = txt[1]
-    mbl = re.sub(r"[^\w\s]",'',s)
+    mbl = re.sub(r"[^\w\s]",'',txt[2])
     
     if list(mbl)[0] == "1":
         mbl = "+{0}".format(mbl)
     elif len(mbl) == 10 and re.match('[2-9]',list(mbl)[0]):
         mbl = "+1{0}".format(mbl)
     
-    p = Person.query.filter(Person.mobile == mbl).first()
-    if not p:
-        p = Person(firstname = fname, lastname = lname, mobile = mbl)
-        db.session.add(p)
+    newb = Person.query.filter(Person.mobile == mbl).first()
+    if not newb:
+        newb = Person(firstname = fname, lastname = lname, mobile = mbl)
+        db.session.add(newb)
         db.session.commit()
     
-        send_sms(person=p, msg="Please save me in your contacts as Shopping List. If you text things to me, I will add them to the shopping list.")
-        send_sms(person=p, msg="You can view the current shopping list at {0}".format(CONFIG['SHOPPING_LIST_URL']))
-        return "Successfully added {0}.".format(p.display_name)
+        send_sms(person=newb, msg="Please save me in your contacts as Shopping List. If you text things to me, I will add them to the shopping list.")
+        send_sms(person=newb, msg="You can view the current shopping list at {0}".format(CONFIG['SHOPPING_LIST_URL']))
+        return "Successfully added {0}".format(newb.display_name)
     else:
-        return "{0} already exists with mobile number {1}".format(p.display_name, p.mobile)
+        return "{0} already exists with mobile number {1}".format(newb.display_name, newb.mobile)
     
 def send_sms(person, msg):
     
     try:
         client = TwilioRestClient(CONFIG['TWILIO_ACCOUNT_SID'], CONFIG['TWILIO_AUTH_TOKEN']) 
-        output = client.messages.create(to=person.mobile, from_='TWILIO_NUMBER',body=msg)  
+        output = client.messages.create(to=person.mobile, from_=CONFIG['TWILIO_NUMBER'],body=msg)  
     except Exception, e:
-        print "Unable to send SMS to {0}".format(person.mobile)  
+        print "Unable to send SMS to {0} because {1}".format(person.mobile,e)  
