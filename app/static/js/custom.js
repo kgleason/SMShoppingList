4469b463eb0bd8fec8dc3dd78241edc5b87d9f89
Kirk Gleason <kgleason@bloommg.com>
2015-03-14 21:43:06 -0400

    Javascript is more specific for checkboxes

--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -7,13 +7,12 @@ $(document).ready(function(){
         socket.emit('connect', {data: 'I\'m connected!'});
     });
 
+    // Some special rules to deal with checkboxes
     $("input[type='checkbox'].sync").change(function() {
-      socket.emit('value changed', {who: $(this).attr('id'), data: $(this).is(':checked')});
+      socket.emit('checkbox changed', {who: $(this).attr('id'), data: $(this).is(':checked')});
     });
 
-    socket.on('update value', function(msg) {
-      console.log(msg);
-      console.log('About to change the value of ' + msg.who + ' from ' + $('input#'+msg.who).is(':checked') + ' to ' + msg.data);
+    socket.on('update checkbox', function(msg) {
       $('input#'+msg.who).prop("checked",(msg.data));
     });
 
