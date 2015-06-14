099b9ff198142368d68275fe713b495d4ccbe928
Kirk Gleason <kgleason@gmail.com>
2015-06-13 23:17:36 -0400

    fixing the manager.

--- manage.py
+++ manage.py
@@ -3,7 +3,11 @@ from flask.ext.migrate import MigrateCommand
 
 manager.add_command('db', MigrateCommand)
 
+@manager.command
+def run():
+	socketio.run(app)
+
 app.debug = True
 
 if __name__ == '__main__':
-  socketio.run(app)
+  manager.run()
