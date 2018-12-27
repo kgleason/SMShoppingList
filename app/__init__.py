7373a242accfaca4ac94d747245bf310fe811eb8
Kirk Gleason <kgleason@bloommg.com>
2018-12-26 20:34:21 -0500

    Disabled a warning from SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 socketio = SocketIO(app)
 
