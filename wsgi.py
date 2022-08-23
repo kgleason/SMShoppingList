dafada13d2d0e2674211c7b290b0f1700b87bf4c
Kirk Gleason <kgleason@gmail.com>
2022-08-23 09:58:06 -0400

    force the port to be integer

--- wsgi.py
+++ wsgi.py
@@ -3,5 +3,5 @@ from eventlet import wsgi
 from app import app
 import os
 
-PORT = os.environ.get('PORT', 8000)
+PORT = int(os.environ.get('PORT', 8000))
 wsgi.server(eventlet.listen(('0.0.0.0', PORT)), app)
