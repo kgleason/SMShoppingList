033949e689a621590d5b83e3c93ae9328885f33e
Kirk Gleason <kgleason@gmail.com>
2022-08-23 10:18:39 -0400

    Only need 1 wsgi (I think).

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,7 +1,9 @@
 import eventlet
 from eventlet import wsgi
-from app import app
+from app import app as application
 import os
+
+app = application
 
 PORT = int(os.environ.get('PORT', 8000))
 wsgi.server(eventlet.listen(('localhost', PORT+1)), app)
--- wsgi.py
+++
@@ -1,7 +0,0 @@
-import eventlet
-from eventlet import wsgi
-from app import app
-import os
-
-PORT = int(os.environ.get('PORT', 8000))
-wsgi.server(eventlet.listen(('localhost', PORT+1)), app)
