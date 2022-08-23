1d58dbd954e592e9983e77458c3517672cc52ba4
Kirk Gleason <kgleason@gmail.com>
2022-08-23 10:45:09 -0400

    switch it uwsgi and gevent, rather than gunicorn and eventlet

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app.wsgi $PORT
+web: uwsgi uwsgi.ini
--- app/wsgi.py
+++
@@ -1,9 +0,0 @@
-import eventlet
-from eventlet import wsgi
-from app import app as application
-import os
-
-app = application
-
-PORT = int(os.environ.get('PORT', 8000))
-wsgi.server(eventlet.listen(('localhost', PORT+1)), app)
